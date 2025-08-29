54fe2f9f5db6f50bc1ec3b0394f499af5f024a25
codeInfiltr4tor <bhaitabahi786@gmail.com>
2025-08-29 20:42:45 +0530

    user check

--- Basics/advancePatterns.java
+++ Basics/advancePatterns.java
@@ -58,19 +58,19 @@ public class advancePatterns {
         5 5 5 5 5
         */
 
-        int n = 5;
+        // int n = 5;
 
-        for (int i = 1; i <= n; i++) {
+        // for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(i+" ");
-            }
-            System.out.println();
+        //     for (int j = 1; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i`; j++) {
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println();
 
-        }
+        // }
 
     }
 
